5db3f5f23019c4bcdf06e96abd45005bd96f98a0
Megan Chantosa <meganemmamoore@gmail.com>
2023-03-28 12:23:56 -0700

    upadted print screen on grocery list

--- public/javascripts/groceries.js
+++ public/javascripts/groceries.js
@@ -3,23 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     return document.querySelector(selector).value;
   }
 
-  const setValue = (selector, newValue) => {
-    document.querySelector(selector).value = newValue;
-  }
-
   const form = document.querySelector('form')
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = getValue('input[id="name"]')
     const quantity = getValue('input[id="quantity"]') || 1;
-
+    
     const list = document.querySelector('ul[id="grocery-list"]');
     const listElement = document.createElement('li')
     listElement.textContent = quantity + ' ' + name;
     list.append(listElement);
     
-    setValue('input[id="name"]', '')
-    setValue('input[id="quantity"]', '')
+   form.reset();
   })
 
 })
