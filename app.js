82487d58e0752877ad1a4b9ddec868baa1e70021
Megan Chantosa <meganemmamoore@gmail.com>
2023-03-28 13:09:58 -0700

    created initial start for comany pages

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
   
 app.listen(PORT, (error) =>{
     if(!error){
-      console.log(`Server is Successfully Running, App is listening on port "+ ${PORT}`)
+      console.log(`Server is Successfully Running, App is listening on port ${PORT}`)
     } else{
       console.log("Error occurred, server can't start", error);
     }     
